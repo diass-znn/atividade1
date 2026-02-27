c31daa0ace94771eaf87a26ac037464a40450f84
davi dias <davidias51131@gmail.com>
2026-02-26 21:14:11 -0300

    variaveis sobrenome e numeroCasa alteradas

--- jsfontes/atividade8.js
+++ jsfontes/atividade8.js
@@ -3,9 +3,12 @@ let idade = 15
 let cpf = '98439607396'
 let email = 'Hd8976@gmail.com'
 let rua = ' rua oito'
+let sobrenome = 'oliveira'
+let numeroCasa = 122
 
 console.log(nome);
 console.log(idade);
 console.log(cpf);
 console.log(email);
 console.log(rua);
+console.log(sobrenome)
