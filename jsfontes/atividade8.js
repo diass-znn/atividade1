3c743939c5f237834db72bf1cfa384532cf0b691
davi dias <davidias51131@gmail.com>
2026-02-26 21:04:49 -0300

    criando e atualizando o arquivo atividade8.js

---
+++ jsfontes/atividade8.js
@@ -0,0 +1,11 @@
+let nome = 'davi'
+let idade = 15
+let cpf = '98439607396'
+let email = 'Hd8976@gmail.com'
+let rua = ' rua oito'
+
+console.log(nome);
+console.log(idade);
+console.log(cpf);
+console.log(email);
+console.log(rua);
