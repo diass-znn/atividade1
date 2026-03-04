04756edaf259e144a147763c428c204ad9b9af4e
davi dias <davidias51131@gmail.com>
2026-03-03 21:47:08 -0300

    criando arquivo atividade10.js

---
+++ jsfontes/atividade10.js
@@ -0,0 +1,19 @@
+function ListaMercado(itensLimpesa, itensAlimento, itensFrutas, itensGeladeira, itensPadaria, itensCaixa) {
+
+
+    console.log(`itens limpesa: ${itensLimpesa[0]}, ${itensLimpesa[1]}, ${itensLimpesa[2]}`)
+    console.log(`itens alimento: ${itensAlimento[0]}, ${itensAlimento[1]}, ${itensAlimento[2]}`)
+    console.log(`itens frutas: ${itensFrutas[0]}, ${itensFrutas[1]}, ${itensFrutas[2]}`)
+    console.log(`itens geladeira: ${itensGeladeira[0]}, ${itensGeladeira[1]}, ${itensGeladeira[2]}`)
+    console.log(`itens padaria: ${itensPadaria[0]}, ${itensPadaria[1]}, ${itensPadaria[2]}`)
+    console.log(`itens do caixa: ${itensCaixa[0]}, ${itensCaixa[1]}, ${itensCaixa[2]}`)
+
+
+}
+
+ListaMercado(['detergente', 'candida', 'vasoura'],
+    ['arroz', 'feijão', 'farofa'],
+    ['banana', 'maça', 'melão'],
+    ['refrigerante', 'suco', 'danone'],
+    ['pão', 'mussarela', 'mortadela'],
+    ['dinheiro', 'sistema', 'sacola'])
